50a6639459f94be59d1b862000eb5731a46ce4b6
Chris <chrismorrisrva@gmail.com>
2018-11-02 07:21:51 -0400

    chap 2

---
+++ example2-1/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace example2
+{
+    class UserInput
+    {
+        static void Main()
+        {
+            double ml;
+            string value1;
+            Console.WriteLine("Enter the amount in ml: ");
+            value1 = Console.ReadLine();
+            ml = double.Parse(value1);
+            Console.WriteLine("You entered " + ml + " ml");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ example2-2/UnitConversions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example2_2
+{
+    class UnitConversion
+    {
+        static void Main(string[] args)
+        {
+            double dimesionInches, dimensionFeet, dimensionSqft, dimensionYard;
+
+            String input;
+            Console.WriteLine("Enter dimesnion  in inches");
+            input = Console.ReadLine();
+            dimesionInches = double.Parse(input);
+            Console.WriteLine("Dimension  in inches is {0}", dimesionInches);
+            Console.ReadLine();
+
+            dimensionFeet = dimesionInches / 12;
+            dimensionSqft = dimensionFeet * dimensionFeet;
+            dimensionYard = dimensionSqft / 9;
+
+            Console.WriteLine("Dimensions in feet is {0}ft , in sqft is {1:F2}sqft, in yard is {2:F4}sqyards", dimensionFeet, dimensionSqft, dimensionYard);
+
+            Console.ReadLine();
+        }
+    }
+}
